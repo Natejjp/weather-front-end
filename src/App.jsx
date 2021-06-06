00e45742f89006f5283a0c04194cd0beebdbf379
Nathan Phame <diyf2021@gmail.com>
2021-06-06 19:29:43 -0400

    Html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <main>
+      <div className="test">
+        <input type="text"></input>
+        <button>Search</button>
+        <div>WEATHER</div>
+      </div>
+    </main>
+  )
 }
